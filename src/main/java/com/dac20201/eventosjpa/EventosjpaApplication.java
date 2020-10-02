8d308b09f328e8fce5202609d22b29bc666bff5b
Thales Machado <thalesm@ipiranga.ipiranga>
2020-10-02 17:00:01 -0300

    Initial commit

---
+++ src/main/java/com/dac20201/eventosjpa/EventosjpaApplication.java
@@ -0,0 +1,13 @@
+package com.dac20201.eventosjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventosjpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventosjpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dac20201/eventosjpa/EventosjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dac20201.eventosjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventosjpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
